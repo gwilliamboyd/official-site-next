e560ef622cf371e278f66173bc5e4f4e162d771f
G William Boyd <gwillboyd@gmail.com>
2023-09-20 13:17:43 -0500

    Fixed GitHub button glitch Increased font weight of navbar links to 300
    Also forgot to mention in last commit that font was changed
    from Montserrat to Jost

--- components/work/GitHubButton.tsx
+++ components/work/GitHubButton.tsx
@@ -1,15 +1,17 @@
 import styles from '@/styles/work.module.css'
-type Props = { gitHubLink: string }
+
+type Props = {
+	gitHubLink: string
+}
 
 const GitHubButton = ({ gitHubLink }: Props) => {
 	return (
-		<span className={styles.gitHubButton}>
-			<a
-				href={gitHubLink}
-				target='_blank'>
-				Source Code
-			</a>
-		</span>
+		<a
+			className={styles.gitHubButton}
+			href={gitHubLink}
+			target='_blank'>
+			Source Code
+		</a>
 	)
 }
 
