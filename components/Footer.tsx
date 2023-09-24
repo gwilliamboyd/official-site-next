112d8ac29b86b0d9bde696f52316b2f603670c8a
G William Boyd <gwillboyd@gmail.com>
2023-09-24 12:21:48 -0500

    Footer media queries done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,30 +10,34 @@ const Footer = (props: Props) => {
 				<span className={styles.footerHeading}>
 					Site developed by G William Boyd
 				</span>
-				<span className={styles.footerSkillList}>
+				<div
+					className={styles.footerTextRight}
+					style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
 					Powered by:
-					<FooterSkill
-						iconSrc='/images/icons/next-js-icon-blue.png'
-						skillName='Next.js'
-						dimensions={36}
-					/>
-					<FooterSkill
-						iconSrc='/images/icons/supabase-icon-blue.png'
-						skillName='Supabase'
-						dimensions={28}
-					/>
-					<FooterSkill
-						iconSrc='/images/icons/typescript-icon-blue.png'
-						skillName='Typescript'
-						dimensions={28}
-					/>
-					{/* <span style={{ color: 'var(--light-blue)' }}>and</span> */}
-					<FooterSkill
-						iconSrc='/images/icons/framer-motion-icon-blue.png'
-						skillName='Framer Motion'
-						dimensions={32}
-					/>
-				</span>
+					<span className={styles.footerSkillList}>
+						<FooterSkill
+							iconSrc='/images/icons/next-js-icon-blue.png'
+							skillName='Next.js'
+							dimensions={36}
+						/>
+						<FooterSkill
+							iconSrc='/images/icons/supabase-icon-blue.png'
+							skillName='Supabase'
+							dimensions={28}
+						/>
+						<FooterSkill
+							iconSrc='/images/icons/typescript-icon-blue.png'
+							skillName='Typescript'
+							dimensions={28}
+						/>
+						{/* <span style={{ color: 'var(--light-blue)' }}>and</span> */}
+						<FooterSkill
+							iconSrc='/images/icons/framer-motion-icon-blue.png'
+							skillName='Framer Motion'
+							dimensions={32}
+						/>
+					</span>
+				</div>
 			</div>
 		</section>
 	)
